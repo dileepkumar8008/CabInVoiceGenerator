3d45cbf35bcecd1b3f7d9eeeac3011b63d84f346
nandunarsu <nandinink1947@gmail.com>
2024-02-21 12:53:23 +0530

    created test by raghu

---
+++ cabInVoiceTests/ProgramTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using cabInVoice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cabInVoice.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void totalFairTest()
+        {
+            Console.WriteLine("created ");
+            Console.WriteLine("created ");
+            Assert.Fail();
+        }
+    }
+}
